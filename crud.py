ce4318a1d958f41660ad2399fd2cf309d31a4144
Saksham Kaushik <112015401+sakshamkaushik-14@users.noreply.github.com>
2024-10-06 22:28:40 +0530

    Update crud.py

--- crud.py
+++ crud.py
@@ -1,6 +1,5 @@
 import mysql.connector
 
-# connect to the database server
 try:
     conn = mysql.connector.connect(
         host='127.0.0.1',
@@ -12,31 +11,6 @@ try:
     print('Connection established')
 except:
     print('Connection error')
-
-# create a database on the db server
-# mycursor.execute("CREATE DATABASE indigo")
-# conn.commit()
-
-# create a table
-# airport -> airport_id | code | name | City
-# mycursor.execute("""
-#CREATE TABLE airport(
-    #airport_id INTEGER PRIMARY KEY,
-    #code VARCHAR(10) NOT NULL,
-    #city VARCHAR(50) NOT NULL,
-    #name VARCHAR(255) NOT NULL
-#)
-#""")
-#conn.commit()
-
-# Insert data to the table
-#mycursor.execute("""
-    #INSERT INTO airport VALUES
-    #(1,'DEL','New Delhi','IGIA'),
-    #(2,'CCU','Kolkata','NSCA'),
-    #(3,'BOM','Mumbai','CSMA')
-#""")
-#conn.commit()
 
 # Search/Retrieve
 mycursor.execute("SELECT * FROM airport WHERE airport_id > 1")
